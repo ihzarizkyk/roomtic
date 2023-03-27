54e4ebe91aa6fb77159b6709ccea4b519e8068dc
ihzarizkyk <ihzarizky30@gmail.com>
2023-03-28 03:51:31 +0700

    create migration table for Room and Detail Room

--- app/Http/Controllers/Dashboard/RoomController.php
+++ app/Http/Controllers/Dashboard/RoomController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use App\Models\Room;
+use App\Models\Tool;
 use Illuminate\Http\Request;
 
 class RoomController extends Controller
---
+++ database/migrations/2023_03_27_202223_create_room_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('room_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger("room_id")->nullable();
+            $table->Integer("capacity")->default(0);
+            $table->text("desc")->nullable();
+            $table->text("img")->nullable();
+            $table->dateTime("startbook")->nullable();
+            $table->dateTime("endbook")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('room_details');
+    }
+}
